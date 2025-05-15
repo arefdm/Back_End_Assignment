7a4f106a94f49cf123985f32f412ee70b313383a
Aref <aref.dehghanimo@gmail.com>
2025-05-14 23:21:02 -0700

    log of tables data

--- Assignment2/src/app.js
+++ Assignment2/src/app.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import 'dotenv/config'
+import { getTableData } from './model/get_data'
+
+const emails = await getTableData('public','recipients');
+console.log(emails);
+const rssUrl = await getTableData('public', 'rss_feeds');
+console.log(rssUrl);
