2f09bc14f18822ac0edffb1cb7692112b0531de2
Aref <aref.dehghanimo@gmail.com>
2025-05-15 17:02:57 -0700

    get several rss

--- Assignment2/src/app.js
+++ Assignment2/src/app.js
@@ -1,7 +1,12 @@
 import 'dotenv/config'
 import { getTableData } from './model/get_data.js'
+import { rssReader } from './rss_reader/rssReader.js'
 
-const emails = await getTableData('public','recipients');
-console.log(emails);
+// const emails = await getTableData('public','recipients');
+// console.log(emails);
 const rssUrl = await getTableData('public', 'rss_feeds');
 console.log(rssUrl);
+const rssHtml = await Promise.all(rssUrl.map(async (rss) =>{
+    return await rssReader(rss.rss_url);
+}));
+console.log(rssHtml.join(''));
