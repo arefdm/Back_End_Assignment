7c23a082066d36d6640a61fc6f41447b60f83f12
Aref <aref.dehghanimo@gmail.com>
2025-05-15 18:02:34 -0700

    add email secrets

--- Assignment2/src/config.js
+++ Assignment2/src/config.js
@@ -5,8 +5,20 @@ const POSTGRES_SECRETS = {
     port:process.env.PGPORT,
     database:process.env.PGDATABASE,
     ssl: true
-}
+};
+
+const EMAIL_SECRETS = {
+host: process.env.MAILHOST,
+  port: process.env.MAILPORT,
+  auth: {
+    user: process.env.MAILUSER,
+    pass: process.env.MAILPASSWORD
+  }
+};
+const MYEMAILADDRESS = process.env.MYEMAILADDRESS;
 
 export {
-    POSTGRES_SECRETS
+    POSTGRES_SECRETS,
+    EMAIL_SECRETS,
+    MYEMAILADDRESS
 };
