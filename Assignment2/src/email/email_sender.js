b25fe95f1a94998d060d7fe15b99402757235239
Aref <aref.dehghanimo@gmail.com>
2025-05-15 18:21:15 -0700

    add sender email

--- Assignment2/src/email/email_sender.js
+++ Assignment2/src/email/email_sender.js
@@ -1,4 +1,4 @@
-import {EMAIL_SECRETS} from '../config.js';
+import {EMAIL_SECRETS,MYEMAILADDRESS} from '../config.js';
 import nodemailer from 'nodemailer';
 
 const transport = nodemailer.createTransport(EMAIL_SECRETS);
@@ -6,6 +6,7 @@ const transport = nodemailer.createTransport(EMAIL_SECRETS);
 export async function emailSender(emails, html){
     try {
         const emailInfo = await transport.sendMail({
+            from : MYEMAILADDRESS,
             to : emails.join(', '),
             subject : 'rss feeds',
             html : html
